2d2c8c577b12c1cbbc4cc2b15589fd210e6caa14
paulinetaris <pauline.tarisellier@gmail.com>
2024-08-12 18:04:26 +0200

    Add new projects
    Structure according to study years

--- Bachelor's projects/PA_modeling/Montant_Taris_NC.py
+++ Bachelor's projects/PA_modeling/Montant_Taris_NC.py
@@ -14,13 +14,13 @@ data_tonic = {
 'tau_m':20,
 'a':0,
 'tau_w':30,
-'b':60/(10**12), #on augmente le courant de b lors de la génération d'un potentiel d'action
-'u_reset':-55, #lorsque u atteint thau_reset : réinitialisé à u_reset
-'u_rest':-70, #potentiel de repos
+'b':60/(10**12),#we increase the current of b when generating an action potential
+'u_reset':-55, #when u reaches thau_reset : reset to u_reset
+'u_rest':-70, #rest potential
 'R':500*(10**9),
 'th':-45, 
-'th_rh':-50, #seuil d'excitabilité
-'DeltaT':2, #pente de l'exponentielle ou sharpness
+'th_rh':-50,#excitability threshold
+'DeltaT':2, #slope of the exponential or sharpness
 'step':65/(10**12)
 }
 
@@ -28,13 +28,13 @@ data_adapting = {
 'tau_m':20,
 'a':0,
 'tau_w':100,
-'b':5/(10**12), #on augmente le courant de b lors de la génération d'un potentiel d'action
-'u_reset':-55, #lorsque u atteint thau_reset : réinitialisé à u_reset
-'u_rest':-70, #potentiel de repos
+'b':5/(10**12), #we increase the current of b when generating an action potential
+'u_reset':-55, #when u reaches thau_reset : reset to u_reset
+'u_rest':-70, #rest potential
 'R':500*(10**9),
 'th':-45, 
-'th_rh':-50, #seuil d'excitabilité
-'DeltaT':2, #pente de l'exponentielle ou sharpness
+'th_rh':-50, #excitability threshold
+'DeltaT':2, #slope of the exponential or sharpness
 'step':65/(10**12)
 }
 
@@ -42,41 +42,40 @@ data_bursting = {
 'tau_m':5,
 'a':-0.5/(10**12),
 'tau_w':100,
-'b':7/(10**12), #on augmente le courant de b lors de la génération d'un potentiel d'action
-'u_reset':-46, #lorsque u atteint thau_reset : réinitialisé à u_reset
-'u_rest':-70, #potentiel de repos
+'b':7/(10**12),#we increase the current of b when generating an action potential
+'u_reset':-46, #when u reaches thau_reset : reset to u_reset
+'u_rest':-70, #rest potential
 'R':500*(10**9),
 'th':-45, 
-'th_rh':-50, #seuil d'excitabilité
-'DeltaT':2, #pente de l'exponentielle ou sharpness
+'th_rh':-50, #excitability threshold
+'DeltaT':2, #slope of the exponential or sharpness
 'step':65/(10**12)
 }
 
 def adex (T,dt,I,data):
-    """simule un potentiel transmembranaire pour un courant constant donné"""
+    """simulates a transmembrane potential for a given constant current"""    
     N=np.floor( T/dt ).astype (int)
     v=np.zeros(N)
-    w=np.zeros(N) #courant d'adaptation : modélise les effets de la présence de certains canaux ioniques
+    w=np.zeros(N) #adaptation current: models the effects of the presence of certain ion channels
     v[0]= data['u_rest']
     w[0]=0
     for i in range (N-1):
-        if v[i] > data ['th']:#si on depasse le seuil
+        if v[i] > data ['th']:#if we exceed the threshold
             v[i]=0
             v[i+1]= data ['u_reset']
             w[i+1]=w[i] + data ['b']
-        else :# sinon on resout les EDO
+        else :# otherwise we solve the EDOs
             v[i+1] = v[i] + (dt/data['tau_m']) * (-v[i] + data['u_rest'] + data['DeltaT'] * np.exp((v[i]-data['th_rh'])/data['DeltaT']) - data['R']*w[i] + data['R']*I[i])
             w[i+1]= w[i]+dt* (data['a']*(v[i] - data['u_rest']) - w[i]) / data ['tau_w']
     return ([v, w])
 
 
 #==================================================================
-
-# QUESTION 2 fonction de gain
+# QUESTION 2 gain function
 
 def gain() :
-    """calcule la fonction de gain du modèle AdEx pour le cas tonique"""
-    freq=[] ; amplitude = []
+    """computes the gain function of the AdEx model for the tonic case"""
+    freq=[] ; amplitude=[]
     a = 0
     dt= 0.01
     T = 300
@@ -88,20 +87,20 @@ def gain() :
         I = [b*(i*dt>=0) for i in range(N)]
         [v,w]=adex(T,dt,I,data)
         nombre=0
-        #compteur des PA pour une intensité
+        #PA counter for intensity
         for i in range(N-1):
             if v[i]<-10 and v[i+1]>-10:
                 nombre +=1
-        #sauvegarde de la fréquence
+       #frequency save
         freq.append(nombre/T)
         amplitude.append(b)
     return amplitude,freq
 
 #=====================================================================
 
-#QUESTION 3 fonction amplitude-durée (cas tonique)
+#QUESTION 3 amplitude-duration function (tonic case)
 def amplitude_duree():
-    """calcule la fonction amplitude-durée (cas tonique)"""
+    """calculates the amplitude-duration function (tonic case)"""
     amp = [] ; dur = []
     T = 5
     dt= 0.01
@@ -109,19 +108,19 @@ def amplitude_duree():
     for td in range(1,N):
         PA = False
         a = 0
-        #tant qu'on a pas de PA, on augmente l'intensité I
+       #as long as we don't have PA, we increase the intensity I
         while PA != True :
             a += 1
             b = a/(10**10)
             I = [b*((i>=10) and (i<=10+td)) for i in range(N)]        
             [v,w] = adex(T, dt, I, data_tonic)
-            #on cherche un PA 
+            #we are looking for a PA
             i = 0
             while i < N-1 and PA == False:
                 if v[i]<-40 and v[i+1]>-40: 
                     PA = True
                 i+=1
-        #enregistrer [td,Id]
+#save [td,Id]
         amp.append(b)
         dur.append(td*dt)
     return dur,amp
@@ -131,31 +130,31 @@ def amplitude_duree():
 
 #QUESTION 4
 
-#AJOUTER UN BRUIT
+#ADD NOISE
 
 def adex_random (T,dt,I,data,sigma):
     """simule un potentiel transmembranaire pour un courant constant donné
     avec un bruit brownien"""
     N=np.floor(T/dt).astype (int)
     v=np.zeros(N)
-    w=np.zeros(N) #courant d'adaptation : modélise les effets de la présence de certains canaux ioniques
+    w=np.zeros(N) #adaptation current: models the effects of the presence of certain ion channels
     v[0]= data['u_rest']
     w[0]=0
     for i in range (N-1):
-        if v[i]> data ['th']:#si on depasse le seuil
+        if v[i]> data ['th']:#if we exceed the threshold
             v[i]=0
             v[i+1]= data ['u_reset']
             w[i+1]=w[i] + data ['b']
-        else :# sinon on resout les EDO
+        else :# otherwise we solve the EDOs
             I_alea= sigma *np.random.normal (0 , 1)
             I_det= (dt/data['tau_m']) * (-v[i] + data['u_rest'] + data['DeltaT'] * np.exp((v[i]-data['th_rh'])/data['DeltaT']) - data['R']*w[i] + data['R']*I[i])
             v[i+1] = v[i] + I_det + np.sqrt(dt)*I_alea
             w[i+1]=w[i]+dt* (data['a']*(v[i] - data['u_rest']) - w[i]) / data ['tau_w']
     return ([v, w])
 
-#COMPTER LES TEMPS DE DECLENCHEMENT DES POTENTIELS D'ACTION
+#COUNTING THE TIMES FOR TRIGGERING ACTION POTENTIALS
 def nuage():
-    """calcule les temps de déclenchement des potentiels d'actions avec un bruit brownien"""
+    """calculates action potential firing times with Brownian noise"""    
     declenchement=[]
     essai=[]
     T = 1000
@@ -164,7 +163,7 @@ def nuage():
     N=np.floor(T/dt).astype(int)
     data = data_tonic
     essais = 10
-    for i in range(essais+1): #Pour que ça fasse les 10 essais et pas 9 
+    for i in range(essais+1): #So that it makes 10 attempts and not 9
         I = [data['step']*(i*dt>=0) for i in range(N)]
         [v,w]=adex_random(T,dt,I,data,sigma)
         #compteur des PA pour une intensité
@@ -177,7 +176,7 @@ def nuage():
 
 
 def main():
-    """"création des plots"""
+    """"creation of the plots"""
     exo=int(input("\n 1-modèle adex \n 2-fonction de gain \n 3-fonction amplitude-durée \n 4-bruit brownien \n 5-nuage de points \n Entrez le numéro de la fonction à lancer : "))
     
     T=300
@@ -190,7 +189,7 @@ def main():
         f=int(input("\n 1-tonique \n 2-bursting \n 3-adapting \n Entrez le numéro de la fonction à lancer : "))
 
         #création du courant
-        N=np.floor(T/dt).astype(int) #pour avoir une meilleure précision
+        N=np.floor(T/dt).astype(int) #for better accuracy       
         if f == 1 : data = data_tonic ; titre = "ADEX tonique"
         elif f == 2: data = data_bursting ; titre = "ADEX bursting"
         else : data = data_adapting ; titre = "ADEX adapting"
@@ -198,7 +197,7 @@ def main():
         
         [v,w]=adex(T,dt,I,data)
 
-        #création du plot
+        #creation of the plot
         temps = np.linspace(0.0, T, len(v))
         bottom,top = plt.ylim(-70,10)
         plt.plot(temps,v,label='PTM',linewidth=2)
@@ -212,7 +211,7 @@ def main():
         
         amplitude,freq=gain()
         
-        #création du plot
+        #creation of the plot
         plt.plot(amplitude,freq, 'o')
         plt.xlabel("Amplitude (V)")
         plt.ylabel("Fréquence de PA (1/ms)")
@@ -228,7 +227,7 @@ def main():
         plt.title("Fonction amplitude-durée dans le cas tonique")
         plt.legend()
     
-    #EXERCICE 4 part 1 : création d'un plot
+    #EXERCICE 4 part 1 : creation of a plot
     elif exo==4:
         data = data_tonic
         N=np.floor(T/dt).astype(int)
@@ -236,7 +235,7 @@ def main():
         [v,w]=adex_random(T,dt,I,data,sigma)
         adex_random (T,dt,I,data,sigma)
 
-        #création du plot pour observer l'effet de l'aléatoire sur le graphe
+        ##creation of the plot to observe the effect of randomness on the graph
         temps = np.linspace(0.0, T, len(v))
         bottom,top = plt.ylim(-70,10)
         plt.plot(temps,v, 'k', label='PTM',linewidth=2)
@@ -245,7 +244,7 @@ def main():
         plt.title("ADEX Random")
         plt.legend()
     
-    #EXERCICE 4 part 2 : création du nuage
+    #EXERCICE 4 part 2 : cloud creation
     elif exo==5:
         essais = 10
         declenchement,essai=nuage()
